9f36d2c0c6fe8caa3dc6e7088430108343d93ea3
warat306 <warat_n306@hotmail.comt>
2014-04-03 16:45:25 +0700

    Init Project

---
+++ src/git/TestJava.java
@@ -0,0 +1,11 @@
+package git;
+
+public class TestJava {
+	
+		public static void main(String[] args){
+			System.out.println("Hello World");
+			System.out.println("I'am Warat Nettat");
+			System.out.println("ECP3R 553333013018-3");
+		}
+
+}
